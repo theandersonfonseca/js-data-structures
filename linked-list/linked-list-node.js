cd837f5232cdfc197b0fd6bdcc61762deca601f6
Anderson <andersonfonseca1999@hotmail.com>
2021-10-16 21:52:34 -0300

    Change ES6 export to CommonJS

--- linked-list/linked-list-node.js
+++ linked-list/linked-list-node.js
@@ -1,6 +1,8 @@
-export default class LinkedListNode {
+class LinkedListNode {
   constructor(data, next = null) {
     this.data = data;
     this.next = next;
   }
 }
+
+module.exports = LinkedListNode;
